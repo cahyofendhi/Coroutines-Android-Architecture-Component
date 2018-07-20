b6feb15cd01f329ab79b747f53d28e9b792786de
Bekti Cahyo <Bektic.22@gmail.com>
2018-07-20 08:17:57 +0700

    gradle

--- app/src/main/java/bcr/coroutinesaac/util/ViewModelUtil.kt
+++ app/src/main/java/bcr/coroutinesaac/util/ViewModelUtil.kt
@@ -4,18 +4,18 @@ import android.arch.lifecycle.*
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
 
-inline fun <reified T : ViewModel> FragmentActivity.getViewModel(): T =
-        ViewModelProviders.of(this)[T::class.java]
-
-inline fun <reified T: ViewModel> FragmentActivity.getActivityFactoryViewModel(factory: ViewModelProvider.NewInstanceFactory):
-        T = ViewModelProviders.of(this, factory)[T::class.java]
-
-inline fun <reified T : ViewModel> Fragment.getFragmentViewModel(): T =
-        ViewModelProviders.of(this)[T::class.java]
-
-inline fun <reified T: ViewModel> Fragment.getFactoryViewModel(factory: ViewModelProvider.NewInstanceFactory):
-        T = ViewModelProviders.of(this, factory)[T::class.java]
-
-inline fun <T> LiveData<T>.subscribe(lifecycle: LifecycleOwner, crossinline onChanged: (T) -> Unit) {
-    observe(lifecycle, Observer { it?.run(onChanged) })
-}
+//inline fun <reified T : ViewModel> FragmentActivity.getViewModel(): T =
+//        ViewModelProviders.of(this)[T::class.java]
+//
+//inline fun <reified T: ViewModel> FragmentActivity.getActivityFactoryViewModel(factory: ViewModelProvider.NewInstanceFactory):
+//        T = ViewModelProviders.of(this, factory)[T::class.java]
+//
+//inline fun <reified T : ViewModel> Fragment.getFragmentViewModel(): T =
+//        ViewModelProviders.of(this)[T::class.java]
+//
+//inline fun <reified T: ViewModel> Fragment.getFactoryViewModel(factory: ViewModelProvider.NewInstanceFactory):
+//        T = ViewModelProviders.of(this, factory)[T::class.java]
+//
+//inline fun <T> LiveData<T>.subscribe(lifecycle: LifecycleOwner, crossinline onChanged: (T) -> Unit) {
+//    observe(lifecycle, Observer { it?.run(onChanged) })
+//}
--- app/src/main/java/bcr/coroutinesaac/view/MainActivity.kt
+++ app/src/main/java/bcr/coroutinesaac/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package bcr.coroutinesaac.view
 
 import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -9,8 +10,6 @@ import bcr.coroutinesaac.R
 import bcr.coroutinesaac.databinding.ActivityMainBinding
 import bcr.coroutinesaac.model.Repository
 import bcr.coroutinesaac.util.Util
-import bcr.coroutinesaac.util.debug
-import bcr.coroutinesaac.util.getActivityFactoryViewModel
 import bcr.coroutinesaac.view.adapter.RepositoryAdapter
 import bcr.coroutinesaac.viewmodel.MainViewModel
 
@@ -28,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupViewModel() {
         val factory = MainViewModel.Companion.Factory(application)
-        val viewModel = getActivityFactoryViewModel<MainViewModel>(factory)
+        val viewModel = ViewModelProviders.of(this, factory)[MainViewModel::class.java]
         binding.vm  = viewModel
         viewModel.mDataObservable?.observe(this, Observer<List<Repository>> { t ->
             if (t != null) {
